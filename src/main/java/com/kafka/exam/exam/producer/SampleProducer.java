f7aada9da186ad5182d710aaa1813c96e049bc6c
swsong <ssw890720@gmail.com>
2022-08-06 00:04:09 +0900

    프로듀서 예제 작성

--- src/main/java/com/kafka/exam/exam/producer/SampleProducer.java
+++ src/main/java/com/kafka/exam/exam/producer/SampleProducer.java
@@ -1,6 +1,10 @@
 package com.kafka.exam.exam.producer;
 
+import com.fasterxml.jackson.databind.ser.std.StdKeySerializers;
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 
 import java.util.Properties;
 
@@ -12,6 +16,13 @@ public class SampleProducer {
     public void run() {
         Properties config = new Properties();
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+        // key, value 타입
+        KafkaProducer<String, String> producer = new KafkaProducer<>(config);
+
+        String testMessage = "testMessage";
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("key", testMessage);
     }
 }
